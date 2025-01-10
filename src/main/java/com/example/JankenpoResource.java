2bad3b3ab07c7977cd0809b370497fd5625885b9
André de Paula Terceiro <andre@terceiro.com.br>
2025-01-10 15:58:19 -0300

    feat: injecting the dependency

--- src/main/java/com/example/JankenpoResource.java
+++ src/main/java/com/example/JankenpoResource.java
@@ -10,17 +10,18 @@ import com.example.model.Jankenpo;
 @Path("/jankenpo")
 public class JankenpoResource {
 
+    @Inject
+    Jankenpo jankenpo;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String play(@QueryParam("escolha") String userMove) {
-        Jankenpo jankenpo = new Jankenpo();
-
-        if (userMove == null || userMove.isEmpty() || ! jankenpo.evaluateUserMove(userMove) ) {
+        if (userMove == null || userMove.isEmpty() || ! this.jankenpo.evaluateUserMove(userMove) ) {
             return "Por favor, informe sua jogada: pedra, papel ou tesoura.";
         }
 
-        String computerMove = jankenpo.getComputerMove();
-        String result = jankenpo.determineWinner(userMove.toLowerCase(), computerMove);
+        String computerMove = this.jankenpo.getComputerMove();
+        String result = this.jankenpo.determineWinner(userMove.toLowerCase(), computerMove);
         return String.format("Você jogou: %s | Máquina jogou: %s | Resultado: %s", userMove, computerMove, result);
     }
 }
