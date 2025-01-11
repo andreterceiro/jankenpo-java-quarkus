aef09bd8a27d19d92bd172df673e3e9c7615a76c
André de Paula Terceiro <andre@terceiro.com.br>
2025-01-10 22:17:37 -0300

    feat: adding a template and invoking it through the JankenpoResource.java file

--- src/main/java/com/example/JankenpoResource.java
+++ src/main/java/com/example/JankenpoResource.java
@@ -1,17 +1,25 @@
 package com.example;
 
 import jakarta.inject.Inject;
-
-import com.example.model.Jankenpo;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import com.example.model.Jankenpo;
 
 @Path("/jankenpo")
 public class JankenpoResource {
+
     private final Jankenpo jankenpo;
+
+    @Inject
+    Template index;
 
     @Inject
     public JankenpoResource(Jankenpo jankenpo) {
@@ -19,14 +27,31 @@ public class JankenpoResource {
     }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String play(@QueryParam("escolha") String userMove) {
-        if (userMove == null || userMove.isEmpty() || ! this.jankenpo.evaluateUserMove(userMove) ) {
-            return "Por favor, informe sua jogada: pedra, papel ou tesoura.";
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance play(@QueryParam("escolha") String userMove) {
+        String error = "";
+        String computerMove = "";
+        String result = "";
+
+        if (userMove == null || userMove.isEmpty() || !this.jankenpo.evaluateUserMove(userMove)) {
+            error = "Por favor, informe sua jogada: pedra, papel ou tesoura.";
+        } else {
+            computerMove = this.jankenpo.getComputerMove();
+            result = this.jankenpo.determineWinner(userMove, computerMove);
         }
 
-        String computerMove = this.jankenpo.getComputerMove();
-        String result = this.jankenpo.determineWinner(userMove, computerMove);
-        return String.format("Você jogou: %s | Máquina jogou: %s | Resultado: %s", userMove, computerMove, result);
+        return index
+            .data("winner", result)
+            .data("userMove", userMove)
+            .data("error", error)
+            .data("computerMove", computerMove);
+    }    
+
+    @GET
+    @Path("/test")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance test() {
+        return index.data("message", "Hello, Qute!");
     }
+    
 }
